c851d71d497f7b4be48516633c7bbd5bcc3ecaa1
cabangon.297337 <cabangon.297337@LAB-A-PC20>
2024-02-23 07:44:32 +0800

    Add project files.

---
+++ ComputerAverageApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ComputerAvaregeApp
+{
+    class ComputerAverageProgram
+    {
+        private static double gradess,Average, round;
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter 5 grades separeted by a new line ");
+            for(int i = 0; i <5 ; i++) {
+
+                  double grade = Convert.ToDouble(Console.ReadLine());
+
+                gradess += grade;
+
+                Average = gradess / 5;
+
+                round = Math.Round(Average);
+            }
+           Console.WriteLine("The Average is " + Average + " and round off to " + round);
+        }
+
+          
+
+    }
+}
